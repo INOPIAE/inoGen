157000a4d6bf26367aafc49e74f8b796b77b8021
INOPIAE <m.maengel@inopiae.de>
2025-08-18 14:05:34 +0200

    add: classes for output

---
+++ inoGenDLL/classes/ClsGenDB.vb
@@ -0,0 +1,242 @@
+﻿Imports System.Data
+Imports System.Data.OleDb
+
+Public Class ClsGenDB
+
+    Public connectionString As String = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=""{0}"";", "")
+
+
+    Public Sub New(dbFileString As String)
+        connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=""{0}"";Persist Security Info=True", dbFileString)
+        ' Constructor logic if needed
+    End Sub
+
+    Public Sub FillPerson(ByRef PD As clsAhnentafelDaten.PersonData)
+        Dim strSQL As String = "SELECT
+                tblPerson.*,
+                tblNachname.Nachname,
+                tblKonfession.Konfessionkurz
+            FROM
+                (
+                    tblPerson
+                    LEFT JOIN tblNachname ON tblPerson.tblNachnameID = tblNachname.tblNachnameID
+                )
+                LEFT JOIN tblKonfession ON tblPerson.tblKonfessionID = tblKonfession.tblKonfessionID
+            WHERE tblPersonID = ?"
+        Dim PNAme As String = ""
+        Using conn As New OleDbConnection(connectionString)
+            conn.Open()
+            Using cmd As New OleDbCommand(strSQL, conn)
+                cmd.Parameters.AddWithValue("@tblPersonID", PD.ID)
+
+                Using reader As OleDbDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        PD.Vorname = If(IsDBNull(reader("Vorname")), "", reader("Vorname").ToString())
+                        PD.Nachname = If(IsDBNull(reader("Nachname")), "", reader("Nachname").ToString())
+                        PD.Geschlecht = If(IsDBNull(reader("Sex")), "", reader("Sex").ToString())
+                        PD.Konfession = If(IsDBNull(reader("Konfessionkurz")), "", reader("Konfessionkurz").ToString())
+                        PD.FID = If(IsDBNull(reader("tblFamilieID")), 0, Convert.ToInt32(reader("tblFamilieID")))
+                        PD.PS = If(IsDBNull(reader("PS")), "", reader("PS").ToString())
+                    End If
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Public Sub FillPersonEltern(ByRef PD As clsAhnentafelDaten.PersonData)
+        Dim strSQL As String = "SELECT
+                tblFamilie.*
+            FROM
+                tblFamilie
+            WHERE tblFamilieID = ?"
+        Using conn As New OleDbConnection(connectionString)
+            conn.Open()
+            Using cmd As New OleDbCommand(strSQL, conn)
+                cmd.Parameters.AddWithValue("@tblFamilieID", PD.FID)
+                Using reader As OleDbDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+                        PD.V = If(IsDBNull(reader("tblPersonIDV")), 0, Convert.ToInt32(reader("tblPersonIDV")))
+                        PD.M = If(IsDBNull(reader("tblPersonIDM")), 0, Convert.ToInt32(reader("tblPersonIDM")))
+                    End If
+                End Using
+            End Using
+        End Using
+    End Sub
+
+    Public Sub FillPersonData(ByRef PD As clsAhnentafelDaten.PersonData)
+        FillPerson(PD)
+        FillPersonEltern(PD)
+        FillPersonDaten(PD)
+        If PD.EID > 0 Then
+            FillFamilieDaten(PD)
+        End If
+
+    End Sub
+
+    Public Sub FillPersonDaten(ByRef PD As clsAhnentafelDaten.PersonData)
+        Dim strSQL As String = "SELECT
+                tblEreignis.tblEreignisID,
+                tblEreignis.tblEreignisArtID,
+                tblEreignisArt.EreignisArt AS Ereignis,
+                tblEreignis.DatumText AS Datum,
+                tblEreignis.Datum AS HDatum,
+                tblEreignis.BisDatumText AS BDatum,
+                tblEreignis.BisDatum AS BHDatum,
+                IIf([tblKreis]![Kreis]<>"""",[tblOrt]![Ort] & "" ("" & [tblKreis]![Kreis] & "")"",[tblOrt]![Ort]) AS Ort,
+                tblKonfession.Konfessionkurz AS Konfession,
+                tblEreignis.Referenz,
+                tblEreignis.FSID,
+                tblEreignis.Info
+            FROM
+                (
+                    (
+                        (
+                            tblEreignis
+                            INNER JOIN tblEreignisArt ON tblEreignis.tblEreignisArtID = tblEreignisArt.tblEreignisArtID
+                        )
+                        INNER JOIN tblKonfession ON tblEreignis.tblKonfessionID = tblKonfession.tblKonfessionID
+                    )
+                    INNER JOIN tblOrt ON tblEreignis.tblOrtID = tblOrt.tblOrtID
+                )
+                LEFT JOIN tblKreis ON tblOrt.tblKreisID = tblKreis.tblKreisID
+            WHERE tblPersonID = ? and tblFamilieID = 0
+            ORDER BY
+                tblEreignisArt.Reihenfolge,
+                tblEreignis.Datum"
+        Using conn As New OleDbConnection(connectionString)
+            conn.Open()
+
+            Using cmd As New OleDbCommand(strSQL, conn)
+                cmd.Parameters.AddWithValue("?", PD.ID) 'Parameter einsetzen
+
+                Using rdr As OleDbDataReader = cmd.ExecuteReader()
+                    While rdr.Read()
+                        ' Werte in Variablen einlesen
+                        Select Case rdr("tblEreignisArtID")
+                            Case 1
+                                PD.Geburtsdatum = GetDatum(rdr)
+                                PD.Geburtsort = rdr("Ort").ToString()
+                            Case 2
+                                PD.Taufdatum = GetDatum(rdr)
+                                PD.Taufort = rdr("Ort").ToString()
+                            Case 6
+                                PD.Sterbedatum = GetDatum(rdr)
+                                PD.Sterbeort = rdr("Ort").ToString()
+                            Case 7
+                                PD.Begräbnisdatum = GetDatum(rdr)
+                                PD.Begräbnisort = rdr("Ort").ToString()
+                            Case >= 8
+                                PD.Sonstige = True
+                        End Select
+
+                    End While
+                End Using
+            End Using
+        End Using
+
+    End Sub
+
+    Private Shared Function GetDatum(rdr As OleDbDataReader) As String
+        Dim strDatum As String = ""
+        strDatum = rdr("Datum").ToString()
+        If IsDBNull(rdr("BDatum")) = False Then
+            If rdr("BDatum").ToString.Trim <> "" Then
+                strDatum &= " - " & rdr("BDatum").ToString()
+            End If
+        End If
+        Return strDatum
+    End Function
+
+    Public Sub FillFamilieDaten(ByRef PD As clsAhnentafelDaten.PersonData)
+        Dim strSQL As String = "SELECT
+                tblEreignis.tblEreignisID,
+                tblEreignis.tblEreignisArtID,
+                tblEreignisArt.EreignisArt AS Ereignis,
+                tblEreignis.DatumText AS Datum,
+                tblEreignis.Datum AS HDatum,
+                tblEreignis.BisDatumText AS BDatum,
+                tblEreignis.BisDatum AS BHDatum,
+                IIf([tblKreis]![Kreis]<>"""",[tblOrt]![Ort] & "" ("" & [tblKreis]![Kreis] & "")"",[tblOrt]![Ort]) AS Ort,
+                tblKonfession.Konfessionkurz AS Konfession,
+                tblEreignis.Referenz,
+                tblEreignis.FSID,
+                tblEreignis.Info
+            FROM
+                (
+                    (
+                        (
+                            tblEreignis
+                            INNER JOIN tblEreignisArt ON tblEreignis.tblEreignisArtID = tblEreignisArt.tblEreignisArtID
+                        )
+                        INNER JOIN tblKonfession ON tblEreignis.tblKonfessionID = tblKonfession.tblKonfessionID
+                    )
+                    INNER JOIN tblOrt ON tblEreignis.tblOrtID = tblOrt.tblOrtID
+                )
+                LEFT JOIN tblKreis ON tblOrt.tblKreisID = tblKreis.tblKreisID
+            WHERE tblPersonID = 0 and tblFamilieID = ?
+            ORDER BY
+                tblEreignisArt.Reihenfolge,
+                tblEreignis.Datum"
+        Using conn As New OleDbConnection(connectionString)
+            conn.Open()
+
+            Using cmd As New OleDbCommand(strSQL, conn)
+                cmd.Parameters.AddWithValue("?", PD.EID) 'Parameter einsetzen
+
+                Using rdr As OleDbDataReader = cmd.ExecuteReader()
+                    While rdr.Read()
+                        ' Werte in Variablen einlesen
+                        Select Case rdr("tblEreignisArtID")
+                            Case 3
+                                PD.Heiratdatum = GetDatum(rdr)
+                                PD.Heiratort = rdr("Ort").ToString()
+                            Case 4
+                                PD.KHeiratdatum = GetDatum(rdr)
+                                PD.KHeiratort = rdr("Ort").ToString()
+                            Case 5
+                                PD.Scheidungsdatum = GetDatum(rdr)
+                                PD.Scheidungsort = rdr("Ort").ToString()
+                            Case 8
+                                PD.Verlobungsdatum = GetDatum(rdr)
+                                PD.Verlobungsort = rdr("Ort").ToString()
+                            Case >= 8
+                                PD.Sonstige = True
+                        End Select
+
+                    End While
+                End Using
+            End Using
+        End Using
+
+
+    End Sub
+
+    Public Function PersonenDaten(ID As Int16) As String
+        Dim strSQL As String = "SELECT
+                tblPerson.tblPersonID,
+                tblPerson.PS,
+                tblPerson.Vorname,
+                tblNachname.Nachname
+            FROM
+                tblPerson
+                LEFT JOIN tblNachname ON tblPerson.tblNachnameID = tblNachname.tblNachnameID
+            WHERE tblPersonID = ?"
+        Dim PNAme As String = ""
+        Using conn As New OleDbConnection(connectionString)
+            conn.Open()
+            Using cmd As New OleDbCommand(strSQL, conn)
+                cmd.Parameters.AddWithValue("@tblPersonID", ID)
+
+                Using reader As OleDbDataReader = cmd.ExecuteReader()
+                    If reader.Read() Then
+
+                        PNAme = If(IsDBNull(reader("PS")), "", reader("PS").ToString()) & " " &
+                        If(IsDBNull(reader("Vorname")), "", reader("Vorname").ToString()) & " " &
+                        If(IsDBNull(reader("Nachname")), "", reader("Nachname").ToString().ToUpper)
+                    End If
+                End Using
+            End Using
+        End Using
+        Return PNAme
+    End Function
+End Class
---
+++ inoGenDLL/classes/clsAhnentafelDaten.vb
@@ -0,0 +1,272 @@
+﻿Imports System.Data.OleDb
+Imports System.Diagnostics.Eventing.Reader
+Imports System.IO
+Imports System.Reflection.Emit
+
+Public Class clsAhnentafelDaten
+
+    Public DBPath As String =""
+    Public connectionString As String = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=""{0}"";", "")
+
+    Public cGenDB As inoGenDLL.ClsGenDB
+
+    Public Structure PersonData
+        Public ID As Integer
+        Public PS As String
+        Public Nachname As String
+        Public Vorname As String
+        Public Geschlecht As String
+        Public Geburtsdatum As String
+        Public Geburtsort As String
+        Public Taufdatum As String
+        Public Taufort As String
+        Public Sterbedatum As String
+        Public Sterbeort As String
+        Public Begräbnisdatum As String
+        Public Begräbnisort As String
+        Public Konfession As String
+        Public Heiratdatum As String
+        Public Heiratort As String
+        Public KHeiratdatum As String
+        Public KHeiratort As String
+        Public Scheidungsdatum As String
+        Public Scheidungsort As String
+        Public Verlobungsdatum As String
+        Public Verlobungsort As String
+        Public Sonstige As Boolean
+        Public FID As Integer 'Eltern-Familie ID
+        Public Pos As Long
+        Public Gen As Long
+        Public V As Long
+        Public M As Long
+        Public EP As Long
+        Public EID As Integer 'Eigene Familie ID
+    End Structure
+
+    Public Persons As New List(Of PersonData)
+    Public Kinder As New List(Of PersonData)
+    Public Ehe As New List(Of Integer)
+    Public Property RootPersonID As Integer = 0
+
+    Public Sub New(dbFileString As String)
+        DBPath = dbFileString
+        connectionString = String.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=""{0}"";", DBPath)
+        cGenDB = New inoGenDLL.ClsGenDB(DBPath)
+    End Sub
+    Public Function GetPersonByID(id As Integer) As PersonData?
+        For Each person In Persons
+            If person.ID = id Then
+                Return person
+            End If
+        Next
+        Return Nothing ' Person nicht gefunden
+    End Function
+
+    Public Sub AddPerson(person As PersonData)
+        Persons.Add(person)
+    End Sub
+
+    Public Sub NewList()
+        Persons.Clear()
+        Dim rootP As New PersonData
+        rootP.ID = RootPersonID
+        rootP.Pos = 1
+        rootP.Gen = 1
+        cGenDB.FillPersonData(rootP)
+        Persons.Add(rootP)
+        Dim index As Integer = 0
+
+        While index < Persons.Count
+            Dim p = Persons(index)
+
+            If p.V > 0 Then
+                Dim newP As New PersonData
+                newP.ID = p.V
+                newP.Pos = p.Pos * 2
+                newP.Gen = p.Gen + 1
+                newP.EID = p.FID
+                cGenDB.FillPersonData(newP)
+                Persons.Add(newP)
+            End If
+
+            If p.M > 0 Then
+                Dim newP As New PersonData
+                newP.ID = p.M
+                newP.Pos = p.Pos * 2 + 1
+                newP.Gen = p.Gen + 1
+                newP.EID = p.FID
+                cGenDB.FillPersonData(newP)
+                Persons.Add(newP)
+            End If
+
+            index += 1
+        End While
+    End Sub
+
+    Public Function NewPerson(ID As Long, Pos As Long) As PersonData
+        Dim np As New PersonData()
+        np.ID = ID
+        np.Pos = Pos
+        cGenDB.FillPersonData(np)
+
+
+        Return np
+    End Function
+
+    Public Sub PrintTree()
+        For Each p In Persons.OrderBy(Function(x) x.Pos)
+            Dim gen As Integer = CInt(Math.Log(p.Pos, 2)) ' Generation = Tiefe im Binärbaum
+            Console.WriteLine(New String(" "c, gen * 4) &
+                              $"[{p.Pos}] {p.Vorname} {p.Nachname} (V:{p.V}, M:{p.M})")
+            Debug.Print(New String(" "c, p.Gen * 4) &
+                              $"[{p.Pos}] {p.Vorname} {p.Nachname} (V:{p.V}, M:{p.M})")
+        Next
+    End Sub
+
+    Public Sub WriteTreeToFile(filePath As String)
+        Ehe.Clear()
+        Dim Generation As Integer = 0
+        Using writer As New StreamWriter(filePath, False, System.Text.Encoding.UTF8)
+            For Each p In Persons.OrderBy(Function(x) x.Pos)
+                If Generation <> p.Gen Then
+                    Generation = p.Gen
+                    writer.WriteLine("# " & ToRoman(Generation) & ". Generation")
+                End If
+                writer.WriteLine("# " & p.Pos & ". " & OutputVorname(p.Vorname) & " " & p.Nachname.ToUpper)
+                AusgabePersDetails(writer, p)
+
+                If p.EID > 0 And Ehe.Contains(p.EID) = False Then
+                    If IsNothing(p.Verlobungsdatum) = False Or IsNothing(p.Verlobungsort) = False Then
+                        writer.WriteLine("⚬ " & If(IsNothing(p.Verlobungsdatum), "    ", p.Verlobungsdatum) & " " & If(IsNothing(p.Verlobungsort), "", p.Verlobungsort) & vbCrLf)
+                    End If
+                    If IsNothing(p.Heiratdatum) = False Or IsNothing(p.Heiratort) = False Then
+                        writer.WriteLine("⚭ " & If(IsNothing(p.Heiratdatum), "    ", p.Heiratdatum) & " " & If(IsNothing(p.KHeiratort), "", p.KHeiratort) & vbCrLf)
+                    End If
+                    If IsNothing(p.KHeiratdatum) = False Or IsNothing(p.KHeiratort) = False Then
+                        writer.WriteLine("♁⚭ " & If(IsNothing(p.KHeiratdatum), "    ", p.KHeiratdatum) & " " & If(IsNothing(p.KHeiratort), "", p.KHeiratort) & vbCrLf)
+                    End If
+                    If IsNothing(p.Scheidungsdatum) = False Or IsNothing(p.Scheidungsort) = False Then
+                        writer.WriteLine("⚮ " & If(IsNothing(p.Scheidungsdatum), "    ", p.Scheidungsdatum) & " " & If(IsNothing(p.Scheidungsort), "", p.Scheidungsort) & vbCrLf)
+                    End If
+                    writer.WriteLine("## Kinder")
+                    AddChildren(p.EID)
+                    For Each k In Kinder.OrderBy(Function(x) x.Pos)
+                        writer.WriteLine("## " & k.Pos & " " & OutputVorname(k.Vorname) & " " & k.Nachname.ToUpper)
+                        If GetPersonByID(k.ID) Is Nothing Then
+                            AusgabePersDetails(writer, k)
+                        Else
+                            writer.WriteLine("siehe oben" & vbCrLf)
+                        End If
+                        Ehe.Add(p.EID)
+                    Next
+                End If
+            Next
+        End Using
+    End Sub
+
+    Public Sub AusgabePersDetails(writer As StreamWriter, p As PersonData)
+        If IsNothing(p.Geburtsdatum) = False Or IsNothing(p.Geburtsort) = False Then
+            writer.WriteLine("∗ " & If(IsNothing(p.Geburtsdatum), "    ", p.Geburtsdatum) & " " & If(IsNothing(p.Geburtsort), "", p.Geburtsort) & vbCrLf) '★
+        End If
+        If IsNothing(p.Taufdatum) = False Or IsNothing(p.Taufort) = False Then
+            writer.WriteLine("~ " & If(IsNothing(p.Taufdatum), "    ", p.Taufdatum) & " " & If(IsNothing(p.Taufort), "", p.Taufort) & vbCrLf)
+        End If
+        If IsNothing(p.Sterbedatum) = False Or IsNothing(p.Sterbeort) = False Then
+            writer.WriteLine("† " & If(IsNothing(p.Sterbedatum), "    ", p.Sterbedatum) & " " & If(IsNothing(p.Sterbeort), "", p.Sterbeort) & vbCrLf)
+        End If
+        If IsNothing(p.Begräbnisdatum) = False Or IsNothing(p.Begräbnisort) = False Then
+            writer.WriteLine("⚰ " & If(IsNothing(p.Begräbnisdatum), "    ", p.Begräbnisdatum) & " " & If(IsNothing(p.Begräbnisort), "", p.Begräbnisort) & vbCrLf)
+        End If
+    End Sub
+
+    Public Sub AddChildren(FID As Integer)
+        Dim strSQL As String = "SELECT
+                tblPersonID,
+                PS,
+                Sex,
+                Vorname
+            FROM
+                tblPerson
+            WHERE tblFamilieID = ?
+
+            ORDER BY Right(PS, 4);"
+
+        Kinder.Clear()
+        Dim Pos As Long = 1
+        Using conn As New OleDbConnection(connectionString)
+            conn.Open()
+
+            Using cmd As New OleDbCommand(strSQL, conn)
+                cmd.Parameters.AddWithValue("?", FID) 'Parameter einsetzen
+
+                Using rdr As OleDbDataReader = cmd.ExecuteReader()
+                    While rdr.Read()
+                        Dim newP As New PersonData
+                        newP.ID = rdr("tblPersonID")
+                        newP.Pos = Pos
+                        cGenDB.FillPersonData(newP)
+                        Kinder.Add(newP)
+                        Pos += 1
+                    End While
+                End Using
+            End Using
+        End Using
+
+    End Sub
+
+    Public Function OutputVorname(vorname As String) As String
+        Dim words As List(Of String) = vorname.Split(" "c).ToList()
+        Dim output As New List(Of String)()
+
+        Dim i As Integer = 0
+        While i < words.Count
+            If words(i).StartsWith("*") Then
+                If i <= words.Count Then
+                    Dim nextWord = words(i)
+                    output.Add(nextWord & "*")
+                    i += 1
+                    Continue While
+                End If
+            End If
+            output.Add(words(i))
+            i += 1
+        End While
+
+        Dim result As String = String.Join(" ", output)
+        Return result
+    End Function
+
+    Public Function ToRoman(ByVal number As Integer) As String
+        If number <= 0 Or number > 3999 Then
+            Throw New ArgumentOutOfRangeException("number", "Nur die Werte 1 - 3999 sind erlaubt.")
+        End If
+
+        Dim romanNumerals As (Value As Integer, Symbol As String)() = {
+        (1000, "M"),
+        (900, "CM"),
+        (500, "D"),
+        (400, "CD"),
+        (100, "C"),
+        (90, "XC"),
+        (50, "L"),
+        (40, "XL"),
+        (10, "X"),
+        (9, "IX"),
+        (5, "V"),
+        (4, "IV"),
+        (1, "I")
+    }
+
+        Dim result As New System.Text.StringBuilder()
+
+        For Each rn In romanNumerals
+            While number >= rn.Value
+                result.Append(rn.Symbol)
+                number -= rn.Value
+            End While
+        Next
+
+        Return result.ToString()
+    End Function
+
+End Class
