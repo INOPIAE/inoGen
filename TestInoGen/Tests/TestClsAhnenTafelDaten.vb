a165d70539ab60f7acd2670de2026200c689e93a
INOPIAE <m.maengel@inopiae.de>
2025-08-25 07:07:57 +0200

    add: unit tests

---
+++ TestInoGen/Tests/TestClsAhnenTafelDaten.vb
@@ -0,0 +1,107 @@
+﻿Imports System.IO
+Imports NUnit.Framework
+Imports inoGenDLL
+
+Namespace TestInoGen
+    Public Class TestClsAhnenTafelDaten
+        Private testPath As String = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)).Replace("\bin", ""), "TestData")
+        Private DBFile As String = testPath & "\Beethoven.inoGdb"
+        Private cAT As inoGenDLL.clsAhnentafelDaten
+        Private cHelper As New ClsHelper
+        Private testFolder As String
+
+
+        <SetUp>
+        Public Sub Setup()
+            testFolder = cHelper.CreateTestFolder("currenttestdata")
+            cHelper.DeleteTestFolder(testFolder)
+            Dim DBFileTest As String = testFolder & "\Beethoven.inoGdb"
+            File.Copy(DBFile, DBFileTest)
+            cAT = New inoGenDLL.clsAhnentafelDaten(DBFileTest)
+        End Sub
+
+        <TearDown>
+        Public Sub TearDown()
+            cHelper.DeleteTestFolder(testFolder)
+        End Sub
+
+        <Test>
+        Public Sub TestFillPerson()
+            cAT.RootPersonID = 1
+            cAT.NewList()
+
+            Assert.That(cAT.Persons.Count, NUnit.Framework.Is.EqualTo(7))
+
+        End Sub
+
+        <Test>
+        Public Sub TestAusgabePersDetails()
+            cAT.RootPersonID = 1
+            cAT.NewList()
+            Dim pd = cAT.GetPersonByID(1)
+            Dim filePath As String = Path.Combine(testFolder, "test.md")
+            Using writer As New StreamWriter(filePath, False, System.Text.Encoding.UTF8)
+                cAT.AusgabePersDetails(writer, pd)
+            End Using
+
+            Dim lines As String() = File.ReadAllLines(filePath)
+
+            Dim expected As String() = {
+                "~ 17.12.1770 Bonn",
+                "† 26.03.1827 Wien",
+                "⚰ 29.03.1827 Wien"
+            }
+
+            For Each line In expected
+                Assert.That(lines, Does.Contain(line), $"Zeile fehlt: {line}")
+            Next
+        End Sub
+
+        <Test>
+        Public Sub TestOutputVorname()
+            Dim Vorname As String = "Karl *Walter Dieter"
+
+            Dim result As String = cAT.OutputVorname(Vorname)
+
+            Assert.That(result, Does.Contain("*Walter*"))
+
+
+            Vorname = "Martin Heinrich August Hermann *Erich"
+
+            result = cAT.OutputVorname(Vorname)
+
+            Assert.That(result, Does.Contain("*Erich*"))
+        End Sub
+
+        <Test>
+        Public Sub TestToRoman()
+            Dim testValue As Integer = 1999
+
+            Dim result As String = cAT.ToRoman(testValue)
+
+            Assert.That(result, [Is].EqualTo("MCMXCIX"))
+
+            testValue = 1
+
+            result = cAT.ToRoman(testValue)
+
+            Assert.That(result, [Is].EqualTo("I"))
+        End Sub
+
+        <Test>
+        Public Sub TestToRoman_Exception()
+
+            Dim ex = Assert.Throws(Of ArgumentOutOfRangeException)(Sub() cAT.ToRoman(4000))
+            Assert.That(ex.Message, Does.Contain("1 - 3999"))
+
+            ex = Assert.Throws(Of ArgumentOutOfRangeException)(Sub() cAT.ToRoman(0))
+            Assert.That(ex.Message, Does.Contain("1 - 3999"))
+
+            ex = Assert.Throws(Of ArgumentOutOfRangeException)(Sub() cAT.ToRoman(5000))
+            Assert.That(ex.Message, Does.Contain("1 - 3999"))
+
+            ex = Assert.Throws(Of ArgumentOutOfRangeException)(Sub() cAT.ToRoman(-1))
+            Assert.That(ex.Message, Does.Contain("1 - 3999"))
+        End Sub
+    End Class
+End Namespace
---
+++ TestInoGen/Tests/TestClsGenDB.vb
@@ -0,0 +1,136 @@
+﻿Imports System.IO
+Imports NUnit.Framework
+Imports inoGenDLL
+
+Namespace TestInoGen
+    Public Class TestClsGenDB
+        Private testPath As String = Path.Combine(Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)).Replace("\bin", ""), "TestData")
+        Private DBFile As String = testPath & "\Beethoven.inoGdb"
+        Private cGDB As inoGenDLL.ClsGenDB
+        Private cHelper As New ClsHelper
+        Private testFolder As String
+
+
+        <SetUp>
+        Public Sub Setup()
+            testFolder = cHelper.CreateTestFolder("currenttestdata")
+            cHelper.DeleteTestFolder(testFolder)
+
+            Dim DBFileTest As String = testFolder & "\Beethoven.inoGdb"
+            File.Copy(DBFile, DBFileTest)
+            cGDB = New inoGenDLL.ClsGenDB(DBFileTest)
+        End Sub
+
+        <TearDown>
+        Public Sub TearDown()
+            cHelper.DeleteTestFolder(testFolder)
+        End Sub
+
+        <Test>
+        Public Sub TestFillPerson()
+            Dim pd = New inoGenDLL.clsAhnentafelDaten.PersonData With {.ID = 1}
+            cGDB.FillPerson(pd)
+
+            Assert.That(pd.Vorname, NUnit.Framework.Is.EqualTo("Ludwig"))
+            Assert.That(pd.Nachname, NUnit.Framework.Is.EqualTo("van Beethoven"))
+            Assert.That(pd.Geschlecht, NUnit.Framework.Is.EqualTo("m"))
+            Assert.That(pd.Konfession, NUnit.Framework.Is.EqualTo("rk"))
+            Assert.That(pd.FID, NUnit.Framework.Is.EqualTo(2))
+            Assert.That(pd.PS, NUnit.Framework.Is.EqualTo("VAN LUDW1770"))
+
+        End Sub
+
+        <Test>
+        Public Sub TestFillPersonEltern()
+            Dim pd = New inoGenDLL.clsAhnentafelDaten.PersonData With {.ID = 1}
+            pd.FID = 1
+            cGDB.FillPersonEltern(pd)
+
+            Assert.That(pd.V, NUnit.Framework.Is.EqualTo(3))
+            Assert.That(pd.M, NUnit.Framework.Is.EqualTo(4))
+
+
+        End Sub
+
+        <Test>
+        Public Sub TestFillPersonDaten()
+            Dim pd = New inoGenDLL.clsAhnentafelDaten.PersonData With {.ID = 1}
+            cGDB.FillPersonDaten(pd)
+
+            Assert.That(pd.Geburtsdatum, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.Geburtsort, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.Taufdatum, NUnit.Framework.Is.EqualTo("17.12.1770"))
+            Assert.That(pd.Taufort, NUnit.Framework.Is.EqualTo("Bonn"))
+            Assert.That(pd.Sterbedatum, NUnit.Framework.Is.EqualTo("26.03.1827"))
+            Assert.That(pd.Sterbeort, NUnit.Framework.Is.EqualTo("Wien"))
+        End Sub
+
+        <Test>
+        Public Sub TestFillPersonData()
+            Dim pd = New inoGenDLL.clsAhnentafelDaten.PersonData With {.ID = 1}
+            cGDB.FillPersonData(pd)
+
+            Assert.That(pd.Vorname, NUnit.Framework.Is.EqualTo("Ludwig"))
+            Assert.That(pd.Nachname, NUnit.Framework.Is.EqualTo("van Beethoven"))
+            Assert.That(pd.Geschlecht, NUnit.Framework.Is.EqualTo("m"))
+            Assert.That(pd.Konfession, NUnit.Framework.Is.EqualTo("rk"))
+            Assert.That(pd.FID, NUnit.Framework.Is.EqualTo(2))
+            Assert.That(pd.PS, NUnit.Framework.Is.EqualTo("VAN LUDW1770"))
+
+            Assert.That(pd.V, NUnit.Framework.Is.EqualTo(2))
+            Assert.That(pd.M, NUnit.Framework.Is.EqualTo(7))
+
+            Assert.That(pd.Geburtsdatum, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.Geburtsort, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.Taufdatum, NUnit.Framework.Is.EqualTo("17.12.1770"))
+            Assert.That(pd.Taufort, NUnit.Framework.Is.EqualTo("Bonn"))
+            Assert.That(pd.Sterbedatum, NUnit.Framework.Is.EqualTo("26.03.1827"))
+            Assert.That(pd.Sterbeort, NUnit.Framework.Is.EqualTo("Wien"))
+
+
+            pd = New inoGenDLL.clsAhnentafelDaten.PersonData With {.ID = 2}
+            cGDB.FillPersonData(pd)
+
+            Assert.That(pd.Vorname, NUnit.Framework.Is.EqualTo("Johann"))
+            Assert.That(pd.Nachname, NUnit.Framework.Is.EqualTo("van Beethoven"))
+            Assert.That(pd.Geschlecht, NUnit.Framework.Is.EqualTo("m"))
+            Assert.That(pd.Konfession, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.FID, NUnit.Framework.Is.EqualTo(1))
+            Assert.That(pd.PS, NUnit.Framework.Is.EqualTo("VAN JOHA1792"))
+
+            Assert.That(pd.V, NUnit.Framework.Is.EqualTo(3))
+            Assert.That(pd.M, NUnit.Framework.Is.EqualTo(4))
+
+            Assert.That(pd.Geburtsdatum, NUnit.Framework.Is.EqualTo("um 1740"))
+            Assert.That(pd.Geburtsort, NUnit.Framework.Is.EqualTo("Bonn"))
+            Assert.That(pd.Taufdatum, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.Taufort, NUnit.Framework.Is.Null.Or.Empty)
+
+            Assert.That(pd.Sterbedatum, NUnit.Framework.Is.EqualTo("18.12.1792"))
+            Assert.That(pd.Sterbeort, NUnit.Framework.Is.EqualTo("Bonn"))
+
+        End Sub
+
+        <Test>
+        Public Sub TestPersonenDaten()
+            Dim PName As String = cGDB.PersonenDaten(1)
+
+            Assert.That(PName, NUnit.Framework.Is.EqualTo("VAN LUDW1770 Ludwig VAN BEETHOVEN"))
+
+        End Sub
+
+        <Test>
+        Public Sub TestFillFamilieDaten()
+            Dim pd = New inoGenDLL.clsAhnentafelDaten.PersonData With {.ID = 2}
+            cGDB.FillPersonData(pd)
+            pd.EID = 1
+            cGDB.FillFamilieDaten(pd)
+
+            Assert.That(pd.Heiratdatum, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.Heiratort, NUnit.Framework.Is.Null.Or.Empty)
+            Assert.That(pd.KHeiratdatum, NUnit.Framework.Is.EqualTo("07.09.1733"))
+            Assert.That(pd.KHeiratort, NUnit.Framework.Is.EqualTo("Bonn"))
+
+        End Sub
+    End Class
+End Namespace
