4dd180e0c9881e21110b37944116140727149a93
INOPIAE <m.maengel@inopiae.de>
2025-09-03 10:26:14 +0200

    add: windows About and Legal

---
+++ inoGen/forms/AboutWindow.xaml.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Reflection
+
+Public Class AboutWindow
+
+    Public Sub New()
+        InitializeComponent()
+
+        Dim version As String = Assembly.GetExecutingAssembly().GetName().Version.ToString()
+        txtVersion.Text = "Version: " & version
+    End Sub
+
+    Private Sub Ok_Click(sender As Object, e As RoutedEventArgs)
+        Me.Close()
+    End Sub
+
+    Private Sub Legal_Click(sender As Object, e As RoutedEventArgs)
+        Dim win As New Legal()
+        win.ShowDialog()
+    End Sub
+End Class
---
+++ inoGen/forms/Legal.xaml.vb
@@ -0,0 +1,64 @@
+﻿Imports System.Reflection
+Imports System.IO
+Public Class Legal
+    Public Sub New()
+        InitializeComponent()
+
+        Dim packages = ReadNuGetPackages("inoGen.vbproj")
+        lstPackages.ItemsSource = packages
+
+        ' --- Lizenztext laden ---
+        Dim licensePath = System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "assets", "License")
+        If File.Exists(licensePath) Then
+            txtLicense.Text = File.ReadAllText(licensePath)
+        Else
+            txtLicense.Text = "Keine Lizenzdatei gefunden."
+        End If
+    End Sub
+
+    Public Function ReadNuGetPackages(projectFile As String) As List(Of String)
+        Dim result As New List(Of String)
+        Dim baseDir = AppDomain.CurrentDomain.BaseDirectory
+        Dim projectDir = Path.GetFullPath(Path.Combine(baseDir, "..\..\..\"))
+        projectFile = Path.Combine(projectDir, projectFile)
+
+        If Not File.Exists(projectFile) Then
+            result.Add("Projektdatei nicht gefunden: " & projectFile)
+            Return result
+        End If
+
+        Dim doc = XDocument.Load(projectFile)
+
+        Dim packages = From p In doc.Descendants("PackageReference")
+                       Let inc = p.Attribute("Include")?.Value
+                       Let ver = p.Attribute("Version")?.Value
+                       Where inc IsNot Nothing And ver IsNot Nothing
+                       Select New With {.Name = inc, .Version = ver}
+
+        For Each pkg In packages
+            Dim pkgPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.UserProfile),
+            ".nuget", "packages", pkg.Name.ToLower(), pkg.Version
+        )
+
+            Dim nuspec = Directory.GetFiles(pkgPath, "*.nuspec", SearchOption.AllDirectories).FirstOrDefault()
+            Dim license = "(Lizenz nicht gefunden)"
+
+            If nuspec IsNot Nothing AndAlso File.Exists(nuspec) Then
+                Dim nuspecDoc = XDocument.Load(nuspec)
+                Dim licNode = nuspecDoc.Descendants().FirstOrDefault(Function(x) x.Name.LocalName = "license")
+                Dim licUrl = nuspecDoc.Descendants().FirstOrDefault(Function(x) x.Name.LocalName = "licenseUrl")
+
+                If licNode IsNot Nothing Then
+                    license = licNode.Value
+                ElseIf licUrl IsNot Nothing Then
+                    license = licUrl.Value
+                End If
+            End If
+
+            result.Add($"{pkg.Name} - {pkg.Version} - Lizenz: {license}")
+        Next
+
+        Return result
+    End Function
+End Class
